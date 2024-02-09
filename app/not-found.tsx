93cf65de151a1e5803a572852e0c33f95c26c5f8
ruslannn199 <ruslannn199@gmail.com>
2024-02-09 15:46:50 +0300

    feat: add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import Title from '@/components/typography/title';
+import type { NextComponentType, NextPageContext } from 'next';
+import Image from 'next/image';
+
+interface Props {}
+
+const NotFound: NextComponentType<NextPageContext, {}, Props> = (
+  props: Props,
+) => {
+  return (
+    <div className="w-full h-full flex flex-col gap-4 justify-center items-center">
+      <Title className="text-5xl">404 - Страница не найдена</Title>
+      <Image src="/404.svg" width={300} height={300} alt="404" />
+    </div>
+  );
+}
+
+export default NotFound;
